92529ac46f7e5445764f4c005f45e98755de8d88
sunBingXu <it_bx@qq.com>
2021-07-16 10:24:13 +0800

    主分支修改

--- springboot-rabbitmq/src/main/java/com/atguigu/rabbitmq/springbootrabbitmq/SpringbootRabbitmqApplication.java
+++ springboot-rabbitmq/src/main/java/com/atguigu/rabbitmq/springbootrabbitmq/SpringbootRabbitmqApplication.java
@@ -9,7 +9,6 @@ public class SpringbootRabbitmqApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootRabbitmqApplication.class, args);
         System.out.println("开始启动了");
-        System.out.println("fenzhi2已经启动");
         System.out.println("我是主分支");
     }
 
