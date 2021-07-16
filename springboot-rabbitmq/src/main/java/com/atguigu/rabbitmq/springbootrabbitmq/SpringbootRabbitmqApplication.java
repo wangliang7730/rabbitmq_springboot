74b02539ccd1b22e085c9546fa88ec7d3a938e80
sunBingXu <it_bx@qq.com>
2021-07-16 10:22:53 +0800

    冲突

--- springboot-rabbitmq/src/main/java/com/atguigu/rabbitmq/springbootrabbitmq/SpringbootRabbitmqApplication.java
+++ springboot-rabbitmq/src/main/java/com/atguigu/rabbitmq/springbootrabbitmq/SpringbootRabbitmqApplication.java
@@ -8,8 +8,9 @@ public class SpringbootRabbitmqApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootRabbitmqApplication.class, args);
+        System.out.println("开始启动了");
         System.out.println("fenzhi2已经启动");
-        System.out.println("fenzhi2启动类");
+        System.out.println("我是主分支");
     }
 
 }
